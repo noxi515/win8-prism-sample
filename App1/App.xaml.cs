2087f8fb4e5e0e1ac38f03cb84aedca29e31c829
noxi <tzo.ppl@gmail.com>
2017-10-05 20:13:21 +0900

    Add comments

--- App1/App.xaml.cs
+++ App1/App.xaml.cs
@@ -8,6 +8,9 @@ namespace App1
 {
     sealed partial class App : MvvmAppBase
     {
+        // UnityContainerはDIコンテナの一種。DI = Dependency Injection（依存性注入）
+        // Angularだとサービスとかファクトリーの名前を引数名で指定して取得するけれど
+        // UnityコンテナはC#のインターフェースに対して実装クラスを注入する
         private readonly IUnityContainer _container = new UnityContainer();
 
         protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
@@ -15,6 +18,9 @@ namespace App1
             // Called when application launched
 
             // First navigation
+            // PrismはナビゲーションをURLっぽく行う。Main = Views/MainPage
+            // Views/MainPageに対してViewModels/MainPageViewModelが対応するVMクラス。名前空間とクラスの命名規則で引っ張る。
+            // XAMLに mvvm:ViewModelLocator.AutoWireViewModel="True" って書いてあるのが重要。
             NavigationService.Navigate("Main", args.Arguments);
 
             return Task.FromResult(0);
@@ -26,6 +32,8 @@ namespace App1
 
 
             // Register DI components
+            // ViewModelでINavigationServiceがコンストラクタで取れるのは
+            // DIコンテナにここでNavigationServiceを設定しているから
             _container.RegisterInstance(NavigationService);
 
             return Task.FromResult(0);
@@ -33,9 +41,9 @@ namespace App1
 
         protected override object Resolve(Type type)
         {
+            // DIの型解決はこちら
             return _container.Resolve(type);
         }
-
 
     }
 }
